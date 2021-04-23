e14f0b22f126c06dbdaf788c44c9294eba1ea6e1
jerrywu128 <48729140+jerrywu128@users.noreply.github.com>
2021-04-23 15:53:19 +0800

    example.py test upload2

--- example.py
+++ example.py
@@ -6,10 +6,15 @@ from datetime import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials as SAC
 
-
+#ex.py
 EMULATE_HX711=False
-
 referenceUnit = -69
+#up.py--
+GDriveJSON = 'key.json'
+GSpreadSheet = 'Python_Android'
+WaitSecond =2
+count = 1
+#--
 
 if not EMULATE_HX711:
     import RPi.GPIO as GPIO
@@ -37,29 +42,39 @@ hx.reset()
 hx.tare()
 #print("Tare done! Add weight now...")
 
-#def vval():
-#while True:
-    #try:
-val = max(0,int(hx.get_weight(5)))
+
+while True:
+    try:
+        val = max(0,int(hx.get_weight(5)))
         #val = hx.get_weight(5)
-#print(val)  
-hx.power_down()
-hx.power_up()
+        #print(val)  
+        hx.power_down()
+        hx.power_up()
         #time.sleep(0.1)
-    #except (KeyboardInterrupt, SystemExit):
-cleanAndExit()
-    #return(val)
+        #up.py----
+        scope = ["https://spreadsheets.google.com/feeds"]
+        key = SAC.from_json_keyfile_name(GDriveJSON,scope)
+        gc = gspread.authorize(key)
+        #worksheet = gc.open(GSpreadSheet).sheet1
+        worksheet = gc.open_by_key("16tNGUftG-4GqH7POpbudnY49RxU14LC89mtgyN1kwXs").sheet1
+
+        dt = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
+        #worksheet.append_row((dt,count))
+        worksheet.update_acell('A2',dt)
+        worksheet.update_acell('B2',val)
+        print("new")
+        time.sleep(WaitSecond)
+        
+    except (KeyboardInterrupt, SystemExit):
+        cleanAndExit()
     
-#vval()
+    
 #--------------------
 #Upload.py
 
 
-GDriveJSON = 'key.json'
-GSpreadSheet = 'Python_Android'
-WaitSecond =2
-count = 1
 
+'''
 while True:
     try:
         scope = ["https://spreadsheets.google.com/feeds"]
@@ -79,3 +94,4 @@ while True:
         print("can not link google")
         sys.exit()
 
+'''
